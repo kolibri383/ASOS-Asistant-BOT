05794b50baf98d1928917aad6a96e82b8c3e2340
kolibri383 <52973708+kolibri383@users.noreply.github.com>
2020-03-26 00:30:46 +0300

    Update conv.py

--- conv.py
+++ conv.py
@@ -81,8 +81,8 @@ def asos_parser_bot(linksJs, all_urls, valuet, session, soup):
     goods = []
     conuntryList = ['RU', 'GB', 'AU', 'TW', 'HK', 'IL', 'CN', 'TR', 'DE', 'SE', 'FR', 'EE']
     for i in range(len(all_urls)):
-    
-           goods.append({'country':country[i], 'valuet': valuet[i]})
+           
+           goods.append({'country':conuntryList[i], 'valuet': valuet[i]})
            
     a = len(goods)
     return a
